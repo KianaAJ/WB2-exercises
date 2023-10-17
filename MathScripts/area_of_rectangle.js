a888fcf9afbccce929973df58df8793bc90af8c2
thekianajones@outlook.com <thekianajones@outlook.com>
2023-10-17 15:24:27 -0400

    Wrote code for finding area of a rectangle

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,12 @@
+"use strict"
+
+console.log("DOES THIS WORK?")
+
+//output
+//A=wl
+
+var w = 15
+var l = 10
+var A = w * l
+
+console.log("The Area due is " + A)
--- MathScripts/net_worth.js
+++ MathScripts/net_worth.js
@@ -5,7 +5,7 @@ console.log("DOES THIS WORK?")
 //output: Your net worth is some-number.
 // To calculate your net worth, you subtract your total liabilities from your total assets. Total assets will include your investments, savings, cash deposits, and any equity that you have in a home, car, or other similar assets. Total liabilities would include any debt, such as student loans and credit card debt.
 
-var Assets = 50000;
+var Assets = 35000;
 var Liabilities = 10000;
 
 var NetWorth = Assets - Liabilities;
