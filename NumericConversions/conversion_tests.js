e82ba5fe81c3985d340faa4dfa5758595fb2b09e
thekianajones@outlook.com <thekianajones@outlook.com>
2023-10-18 14:32:08 -0400

    Doing Numeric Conversions with JS

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,29 @@
+"use strict"
+
+// Description: This script tests various numeric conversion techniques
+// Author: Kiana Jones (New Programmer)
+
+var a = "  101.1  "
+var b = "55"
+var c = "402 Stevens"
+var d = "Number 5  "
+
+//console.log(parseInt(a)) = 101
+//console.log(parseInt(b)) = 55
+//console.log(parseInt(c)) = 402
+//console.log(parseInt(d)) = NaN
+
+//console.log(parseFloat(a)) = 101.1
+//console.log(parseFloat(b)) = 55
+//console.log(parseFloat(c)) = 402
+//console.log(parseFloat(d)) = NaN
+
+//console.log(Number(a)) = 101.1
+//console.log(Number(b)) = 55
+//console.log(Number(c)) = NaN
+//console.log(Number(d)) = NaN
+
+//console.log(+(a)) = 101.1
+//console.log(+(b)) = 55
+//console.log(+(c)) = NaN
+//console.log(+(d)) = NaN
