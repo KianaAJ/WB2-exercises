d58b9704da4403c5f06bc6276e7fcbd19518f442
thekianajones@outlook.com <thekianajones@outlook.com>
2023-10-18 09:14:25 -0400

    Fixed formula for rule of 72

--- MathScripts/rule_of_72.js
+++ MathScripts/rule_of_72.js
@@ -4,11 +4,12 @@ console.log("DOES THIS WORK?")
 
 //output At a some-number% interest rate, your savings account will be worth some-number in some-number years.
 //formula 72/IR = T
-
-var IR = 6
+var Savings = 400
+var IR = 4
 var T = 72/IR
+var SavingsIncrease = Savings * 2
 
 // t	=	number of periods required to double an investment's value
 // {r}	=	interest rate per period, as a percentage
 
-console.log("The T due is " + T)
+console.log("At a " + IR + "% interest rate, your savings account will be worth $" + SavingsIncrease + " in " + T.toFixed(2) + "years");
